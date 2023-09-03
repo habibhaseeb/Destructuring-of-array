e081d3724c8676e0efbee1fa1cddc2230f3000bc
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-09-03 23:36:52 +0500

    Create funcDestructuring.js

---
+++ funcDestructuring.js
@@ -0,0 +1,12 @@
+//
+function printFullName({ firstName, lastName }) { // fname & 2name value 
+                                                  // .....is in ln #8
+  
+    console.log(`${firstName} ${lastName}`);
+ 
+}
+  
+const person = { firstName: 'John', lastName: 'Doe' };
+ 
+printFullName(person); // John Doe
+  
