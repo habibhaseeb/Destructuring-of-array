c72a296bbc09418341426101bae577cbec110d60
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-09-03 23:37:55 +0500

    Create nested_Obj.js

---
+++ nested_Obj.js
@@ -0,0 +1,21 @@
+const data = {
+    user: {
+
+      name: 'Alice',
+
+      favorites: ['JavaScript', 'React'], // It are two VALUES.
+    },
+  };
+  
+  const {
+
+    user: { name, favorites: [firstFavorite, secondFavorite] },
+ 
+} = data;
+  
+  console.log(name); // Alice
+
+        console.log(firstFavorite); // JavaScript
+
+            console.log(secondFavorite); // React
+  
