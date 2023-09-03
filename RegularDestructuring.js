48efe2550f82baac17d7923b75cb30951257b9e9
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-09-03 23:38:59 +0500

    Create RegularDestructuring.js

---
+++ RegularDestructuring.js
@@ -0,0 +1,19 @@
+// Destructuring in JS is to extract values from array.
+// Destructuring is commonly used in assignments,
+// function parameters, and for iterating over arrays and objects. Now?
+                    // first method
+const [ John , petal , jims ] = [1, 2 , 3];
+
+ console.log(John);
+
+    console.log(petal);
+
+        console.log(jims);
+        
+                    // Second method
+
+  let obj = {Name:'john', age:'16'};
+
+        let res = {class:'12', obj};
+
+            console.log(res);
