56042c8deb5896799a2e8784906ba93a20e097be
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-14 06:47:27 -0700

    Update alias_Obj.js

--- alias_Obj.js
+++ alias_Obj.js
@@ -1,4 +1,13 @@
 const person = { firstName: 'John', lastName: 'Doe' };
+
 const { firstName: first, lastName: last } = person;
+
 console.log(first); // John
 console.log(last); // Doe
+
+const objInJs = {
+     filterNum: Math.abs(dest.at(2));
+     const dest:{ [1,2,9.48,39.5,-736.4,6] };
+ 
+};
+const res = Object.values(objInJs.filterNum);
